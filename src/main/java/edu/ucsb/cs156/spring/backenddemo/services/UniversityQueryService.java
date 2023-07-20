6a4cc27e31e59018217c9a1c5fd1a049a4658383
Andrew Thai <andrewthai@169-231-175-225.wireless.ucsb.edu>
2023-07-20 15:53:16 -0700

    finished controller

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/UniversityQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/UniversityQueryService.java
@@ -1,3 +1,49 @@
+/*package edu.ucsb.cs156.spring.backenddemo.services;
+
+
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.web.client.RestTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+
+@Slf4j
+@Service
+public class UniversityQueryService {
+
+
+    private final RestTemplate restTemplate;
+
+    public UniversityQueryService(RestTemplateBuilder restTemplateBuilder) {
+        restTemplate = restTemplateBuilder.build();
+    }
+
+    public static final String ENDPOINT = "http://universities.hipolabs.com/search?name={name}";
+
+    public String getJSON(String name) throws HttpClientErrorException {
+        log.info("name={}",name);
+         var re = restTemplate.getForEntity(ENDPOINT.replace("{name}", name), String.class);
+
+       return re.getBody();
+     
+    }
+}
+*/
+
 package edu.ucsb.cs156.spring.backenddemo.services;
 
 
