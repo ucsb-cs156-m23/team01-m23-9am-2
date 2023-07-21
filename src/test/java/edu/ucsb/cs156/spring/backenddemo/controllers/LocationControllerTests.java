158aafbb69dde0f70ab1833df892db57942c56f0
Mohamed-Elfouly <elfouly@ucsb.edu>
2023-07-21 11:17:13 -0700

    Update LocationControllerTests.java

--- src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationControllerTests.java
+++ src/test/java/edu/ucsb/cs156/spring/backenddemo/controllers/LocationControllerTests.java
@@ -45,7 +45,7 @@ public class LocationControllerTests {
     String location = "Chicago";
     when(mockLocationQueryService.getJSON(eq(location))).thenReturn(fakeJsonResult);
 
-    String url = String.format("/api/Location/get?location=%s",location);
+    String url = String.format("/api/location/get?location=%s",location);
 
     MvcResult response = mockMvc
         .perform( get(url).contentType("application/json"))
