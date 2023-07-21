e420a5df8cfb36dd628f34f64a1ce164e547a9a4
Shiyang/Bill <shiyangxin@ucsb.edu>
2023-07-21 11:38:46 -0700

    Update ZipCodeController.java

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/ZipCodeController.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/controllers/ZipCodeController.java
@@ -1,38 +1,41 @@
 package edu.ucsb.cs156.spring.backenddemo.controllers;
-import edu.ucsb.cs156.spring.backenddemo.services.CountryCodeQueryService;
-import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import edu.ucsb.cs156.spring.backenddemo.services.ZipCodeQueryService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
-@Tag(name="Country Code info from http://api.zippopotam.us/us/{zipcode}")
+import lombok.extern.slf4j.Slf4j;
+
+@Tag(name="zipcode")
 @Slf4j
 @RestController
-@RequestMapping("/api/zipcode/get")
+@RequestMapping("/api/zipcode")
 public class ZipCodeController {
+
     ObjectMapper mapper = new ObjectMapper();
 
     @Autowired
-    ZipCodeQueryService zipCodeQueryService;
+    ZipCodeQueryService ZipCodeQueryService;
 
-    @Operation(summary="Get an area's zip code and more", description ="zip code data uploaded to zippopotam")
+    @Operation(summary = "Get Zipcode Info", description = "Zipcode Info from http://www.zippopotam.us/")
     @GetMapping("/get")
-    public ResponseEntity<String> getZipCodes(
-        @Parameter(name="zipcode", example="93117") @RequestParam String zipcode
+    public ResponseEntity<String> getzipcode(
+        @Parameter(name="zipcode", description="zipcode, e.g. 93117") @RequestParam String zipcode
     ) throws JsonProcessingException {
-        log.info("getzipCodes: zipcode={}", zipcode);
-        String result = zipCodeQueryService.getJSON(zipcode);
+        log.info("getzipcode: zipcode={}", zipcode);
+        String result = ZipCodeQueryService.getJSON(zipcode);
         return ResponseEntity.ok().body(result);
     }
 
 }
-
